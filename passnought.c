3086f23685b4070ba8841eae455de63e8c611cf3
kstacks <54756161+Ksstacks@users.noreply.github.com>
2024-07-25 12:44:54 -0400

    Add files via upload

---
+++ passnought.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    scanf("How long do you want to?: ");
+    for(int i = 0; i <= 10; i++)
+    {
+        printf("Welcome to Online IDE!! Happy Coding :)\n");
+    }
+    return 0;
+}
